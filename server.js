8979fcd79a91088808841ea820a34ce0ebb07dec
560h <gangtoolsdiscord@gmail.com>
2025-03-29 23:12:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins during development
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
